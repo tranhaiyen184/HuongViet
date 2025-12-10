31fd20d4cc53a15a80c71897cd47843a86b0d6e4
Phong Ly <viphongly2804@gmail.com>
2025-12-10 19:41:02 +0700

    Add database backup functionality to BLL and DAL
    Added `BackupBLL` to the business logic layer for managing
    database backups, leveraging the new `BackupRepository`
    class in the data access layer. The `BackupRepository`
    uses the `mysqldump` utility to create database dumps.
    
    Updated `HuongViet.BLL.csproj` and `HuongViet.DAL.csproj`
    to include the new files. Minor formatting adjustments
    were made to `HuongViet.BLL.csproj` without functional
    changes.

---
+++ HuongViet.BLL/BackupBLL.cs
@@ -0,0 +1,27 @@
+﻿using HuongViet.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuongViet.BLL
+{
+    public class BackupBLL
+    {
+        private readonly BackupRepository _repo;
+
+        public BackupBLL(BackupRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void BackupDatabase(string destinationPath)
+        {
+            if (string.IsNullOrWhiteSpace(destinationPath))
+                throw new ArgumentException("Backup file path is required.");
+
+            _repo.BackupTo(destinationPath);
+        }
+    }
+}
